319b06e374733b0c853555cca59e281b0f0ce7a7
sonucoderai <sonuyadav07122008@gmail.com>
2025-11-17 17:06:29 +0530

    Create TYPE CASTING.C

---
+++ TYPE CASTING.C
@@ -0,0 +1,14 @@
+3.TYPE CASTING;
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20;
+    int sum = a + b;
+    
+    printf("sum = %d",sum);
+    return 0;
+}
+
+
