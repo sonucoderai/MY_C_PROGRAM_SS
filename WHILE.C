fd7e8c89964deabb7f75cbafb9be1e3e89f6e4b6
sonucoderai <sonuyadav07122008@gmail.com>
2025-11-18 11:50:33 +0530

    Create WHILE.C

---
+++ WHILE.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    // Print numbers from 1 to 5
+    while(i <= 5) {
+        printf("%d\n", i);
+        i++;   // increment i
+    }
+
+    return 0;
+}
