80ecd3d48d304739bdf19e0d1f7116f680c10e90
sonucoderai <sonuyadav07122008@gmail.com>
2025-11-18 16:29:38 +0530

    Create USER DEFINED SI.C

---
+++ USER DEFINED SI.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sicalculator(){
+    int p,t,r,si;
+    printf("enter the value of principle\n");
+    scanf("%d",&p);
+    printf("enter year\n");
+    scanf("%d",&t);
+    printf("enter interest\n");
+    scanf("%d",&r);
+    si=(p*t*r)/100;
+    printf("si=%d\n",si);
+;
+}
+   int main(){
+       sicalculator();
+   }
