d177b3b3eb416a9471c254a996e5abe7d690d84f
sonucoderai <sonuyadav07122008@gmail.com>
2025-11-18 12:16:33 +0530

    Create FIBONACCI SERIES.C

---
+++ FIBONACCI SERIES.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n, first = 0, second = 1, next, i;
+    printf("Enter how many terms you want: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        if (i <= 1)
+            next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        printf("%d ", next);
+    }
+
+    return 0;
+}
