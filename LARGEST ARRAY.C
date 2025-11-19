32feff057bbb94e5028650a74682b52bd7273a83
sonucoderai <sonuyadav07122008@gmail.com>
2025-11-19 16:05:44 +0530

    Create LARGEST ARRAY.C

---
+++ LARGEST ARRAY.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int arr[5]={1,2,3,4,5};
+    int max = arr[0];
+    for(int i = 0; i < 5; i++) {
+        if (arr[i]>max){
+        arr[i]=max ;
+    }
+    }
+    printf("Largest element in array = %d\n", max);
+}
+    
