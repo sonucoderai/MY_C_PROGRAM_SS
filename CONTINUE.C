637c4581e60c8a346283a383ba8574b0566f3a4c
sonucoderai <sonuyadav07122008@gmail.com>
2025-11-19 20:40:58 +0530

    Remove comment for continue statement

--- CONTINUE.C
+++ CONTINUE.C
@@ -4,7 +4,7 @@ int main() {
     for (int i = 1; i <= 10; i++) {
 
         if (i == 5) {
-            continue;   // skip printing when i becomes 5
+            continue;   
         }
 
         printf("%d\n", i);
